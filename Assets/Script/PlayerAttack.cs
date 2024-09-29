0f2561cbeaf25d8502d80ed158ea958e6dfa166d
jongddak <hfsmsj316@gmail.com>
2024-09-29 13:02:22 +0900

    fead 간단한 맵 구현 및 구역 제한
    간단한 맵 구현 및 구역 제한

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
-{   
-    // 커서?
+{
+    [SerializeField] float atkSpeed;
+    [SerializeField] float atkRange;
+    [SerializeField] int atkCount;
+    [SerializeField] float atk;
+    
 
 
+    public enum AtkType  { Slash, Stap } // 공격 형태  기본은 slash , 처음 스킬에선(5레벨) 찌르기로 변경(변경 + 공격력정도 주면 될듯) or 베기 강화 
+    // 
+    private AtkType curatktype = AtkType.Slash; 
+    private void Start()
+    {
+        StartCoroutine(Attacking(atkCount,atkRange,curatktype,atkSpeed));
+    }
     private void Update()
     {
         Aiming();
+    }
+    IEnumerator Attacking(int atkcount, float atkrange, AtkType atktype, float atkspeed) // 공격속도에 맞게 공격
+    {
+       WaitForSeconds atkTime =  new WaitForSeconds(atkSpeed);
+
+        while (true) 
+        {
+            Attack(atkcount,atkrange,atktype);
+            yield return atkTime;
+        }
+    }
+    private void Attack(int atkcount,float atkrange ,AtkType atktype ) // 애니메이션 + 사운드 + 비주얼 이펙트  플레이어의 선택지에 따라 변화해야함  인수로 공격횟수 범위 형태 ? 
+    {
+        if (atktype == AtkType.Slash)
+        {
+            for (int i = 0; i < atkcount; i++)
+            {
+                // 공격 횟수 만큼 반복해서 공격재생 
+                Debug.Log("베기!");
+            }
+        }
+        else if (atktype == AtkType.Stap)
+        {
+            for (int i = 0; i < atkcount; i++)
+            {
+                // 공격 횟수 만큼 반복해서 공격재생 
+            }
+        }
     }
 
     private void Aiming() 
--- Assets/Script/PlayerMover.cs
+++ Assets/Script/PlayerMover.cs
@@ -10,24 +10,35 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] SpriteRenderer spriteRenderer;
 
+    [SerializeField] Collider2D bound;
 
-    private void Update()
+
+    private Bounds bounds;
+
+    private void Start()
+    {
+        bounds = bound.bounds;
+    }
+
+
+    private void Update()  // fixed? 
     {
         Move();
 
+
     }
 
-    private void Move() 
+    private void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
 
         transform.Translate(Vector2.right * moveSpeed * x * Time.deltaTime);
         transform.Translate(Vector2.up * moveSpeed * y * Time.deltaTime);
 
 
-        if (x < 0) 
+        if (x < 0)
         {
 
             spriteRenderer.flipX = true;
@@ -36,7 +47,16 @@ public class PlayerMover : MonoBehaviour
         {
             spriteRenderer.flipX = false;
         }
+
+        Vector2 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, bounds.min.x,bounds.max.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, bounds.min.y, bounds.max.y);
+
+
+        transform.position = clampedPosition;
     }
+
+
 
 
 }
